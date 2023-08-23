b12bbd78b89f9ae43abfbdc539122db24afebfec
BiancaPuertaRocha <biapuertarocha@gmail.com>
2023-08-23 14:01:49 -0300

    unittest filter by description

--- JosesBarAPI/JosesBarAPI.tests/ProductTests.cs
+++ JosesBarAPI/JosesBarAPI.tests/ProductTests.cs
@@ -10,9 +10,9 @@ namespace JosesBarAPI.tests
     {
         private List<Product> products = new List<Product>()
             {
-                new Product() { Id = 1, Description = "Skol lata 350ml", Price = 2.3m, Quantity = 10},
-                new Product() { Id = 2, Description = "Coca-Cola lata 350ml", Price = 3.50m, Quantity = 15},
-                new Product() { Id = 3, Description = "Suco de Uva 500ml", Price = 5.50m, Quantity = 50}
+                new Product() { Id = 1, Description = "skol lata 350ml", Price = 2.3m, Quantity = 10},
+                new Product() { Id = 2, Description = "coca-Cola lata 350ml", Price = 3.50m, Quantity = 15},
+                new Product() { Id = 3, Description = "suco de Uva 500ml", Price = 5.50m, Quantity = 50}
             };
         private readonly Mock<IProductRepository> _productRepositoryMock;
         private readonly ProductController _productController;
@@ -25,6 +25,7 @@ namespace JosesBarAPI.tests
 
             _productRepositoryMock.Setup(r => r.GetProducts().Result).Returns(products);
             _productRepositoryMock.Setup(r => r.GetProductByID(2).Result).Returns(products.Find(x => x.Id == 2));
+            _productRepositoryMock.Setup(r => r.GetProductByDescription("s").Result).Returns(products.FindAll(x => x.Description.Contains("s")));
         }
 
 
@@ -46,6 +47,18 @@ namespace JosesBarAPI.tests
             var actualResult = okResult.Value as Product;
 
             Assert.Equal(2, actualResult.Id);
+        }
+
+        [Fact]
+        public void List_GetProductByDescription_FilteredProducts()
+        {
+            var result = _productController.GetAsync(description:"s").Result;
+            var okResult = result as OkObjectResult;
+            var actualResult = okResult.Value as List<Product>;
+
+            Assert.Equal(2, actualResult.Count);
+            Assert.All(actualResult, item => Assert.Contains("s", item.Description));
+           
         }
     }
 }
